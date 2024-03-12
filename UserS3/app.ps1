8c01239a2c9ce8272cbccc61c20cc451b9e1eccd
JustinLopuschynski <justin.lopuschynski@plan-b-gmbh.com>
2024-03-12 10:28:51 +0100

    fds

--- UserS3/app.ps1
+++ UserS3/app.ps1
@@ -23,12 +23,12 @@ $parameters = Get-Content -Path $parametersFile | ConvertFrom-Yaml
 Connect-AzAccount
 $subcriptionId = Read-Host 'subcription ID'
 Select-AzSubscription -SubscriptionId $subcriptionId
-$ResourceGroup = "DevOpsPaket"
+
 
 $skuName = $parameters.skuName
 $skuUnits = $parameters.skuUnits
 $d2cPartitions = $parameters.d2cPartitions
-
+$resourceGroup = $parameters.resourceGroup
 
 $bicepParams = @{
     'skuName' = $skuName
@@ -36,7 +36,3 @@ $bicepParams = @{
     'd2cPartitions' = $d2cPartitions
 }
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile "app.bicep" -TemplateParameterObject $bicepParams 
-
-#az deployment group create --resource-group blogBicepAzureIoT --template-file AzureIoTDemo.bicep --parameters demoName=JulyOT
-
-#New-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile "app.json" -TemplateParameterObject $bicepParams -Location "West Europe"
