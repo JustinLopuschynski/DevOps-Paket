3ee50524a606f61cdabefff66e8454f885753875
JustinLopuschynski <justin.lopuschynski@plan-b-gmbh.com>
2024-03-13 09:31:56 +0100

    3.1 update

---
+++ UserS3.1/test_function.ps1
@@ -0,0 +1,14 @@
+# Set variables
+$iotHubName = "<your IoT Hub Name>"
+$deviceId = "<device Name>"
+$message = "$PSScriptRoot\message.json"
+
+
+Connect-AzAccount
+$subcriptionId = Read-Host 'subcription ID'
+Select-AzSubscription -SubscriptionId $subcriptionId
+
+# Send a test message to the IoT Hub
+Write-Host "Sending test message to IoT Hub..."
+az iot device send-d2c-message -n $iotHubName -d $deviceId --props '$.ct=application/json;$.ce=utf-8' --data-file-path $message
+Write-Host "Sending complete"
